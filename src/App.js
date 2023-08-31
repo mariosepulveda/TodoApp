c984e2de0d355908335773bd7880e30ffdeaf692
mariosepulveda <masepulvedar@uqvirtual.edu.co>
2023-08-31 08:01:21 -0500

    Update App.jssssss

--- src/App.js
+++ src/App.js
@@ -5,31 +5,34 @@ import TodoList from './Components/TodoList';
 import TodoItem from './Components/TodoItem';
 import { TodoCreateButton } from './Components/TodoCreateButton';
 
-/**
- * 
- * const arrayTareas = [
-  {description:'Taller de Inglés básico sobre elementos de trabajo.', complete:false},
-  {description:'Curso para conseguir trabajo en EEUU.', complete:false},
-  {description:'Curso de inglés intermedio para el trabajo.', complete:false},
-  {description:'Curso de introducción a React.js', complete:false},
-  {description:'Estudiar Inglés A2', complete:false},
-  {description:'Terminar Hoja de vida', complete:false},
-];
- */
+/* const arrayTareas = [
+  {description:'Pedir las vacaciones en la empresa.', complete:false},
+  {description:'Comprar el Pan Tajado.', complete:false},
+  {description:'Arreglar hoja de vida.', complete:false},
+  {description:'Tender la cama', complete:false},
+  {description:'Almorzar', complete:false},
+  {description:'Hacer jugo de guanabana', complete:false},
+  {description:'Comprar Tomates y frutas para jugo', complete:false}
+]; */
+//
+//localStorage.setItem('TODOS_ARRAY',arrayTareas);
+
+localStorage.removeItem('TODOS_ARRAY');
 
 function App() {
-  const localStorageTodos=localStorage.getItem('TODOS_V1')
-  let parsedTodos;
-  if(!localStorageTodos){
-    localStorage.setItem('TODOS_V1',JSON.stringify([]));
-    parsedTodos=[];
-  }else{
-    parsedTodos=JSON.parse(localStorageTodos);
-  }
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
 
+  let parsedTodos = [];
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    //parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }  
   const [todos,setTodos] = React.useState(parsedTodos);
+
   const [searchValue,setSearchValue] = React.useState('');
-  
   
   const totalTodos = todos.length;
   const completeTodos = todos.filter(todo=>!!todo.complete).length;
@@ -37,19 +40,29 @@ function App() {
    return todo.description.toLowerCase().includes(searchValue.toLowerCase());
   });
 
+
+  const savesTodos = (newTodos) => {
+    setTodos(newTodos);
+    localStorage.setItem('TODOS_V1',JSON.stringify(newTodos));
+  }
+
   const completeTodo = (description) => { 
     const newTodos = [...todos]; 
     const todoIndex = newTodos.findIndex((todo) => todo.description === description);
     newTodos[todoIndex].complete = !newTodos[todoIndex].complete; 
-    setTodos(newTodos); 
+    //setTodos(newTodos); 
+    savesTodos(newTodos);
   };
 
   const deleteTodo = (key) => {
     const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex((todo) => todo.key === key);
+    const todoIndex = newTodos.findIndex((todo) => todo.description === key);
+    //console.log('array:\n'+newTodos);
+    //console.log('elemento a eliminar:',todoIndex);
     newTodos.splice(todoIndex,1);
-    setTodos(newTodos);
-    //saveTodos(newTodos); 
+    //setTodos(newTodos); 
+    savesTodos(newTodos);
+
   }
  
   return (
@@ -65,7 +78,7 @@ function App() {
           description={todo.description}
           complete={todo.complete}
           onComplete={()=>completeTodo(todo.description)}
-          onDelete={()=>deleteTodo(todo.key)}
+          onDelete={()=>deleteTodo(todo.description)}
           />
         ))}
       </TodoList>
