57667823598b4acf8a8c7554053b2bc3079f1ea7
mariosepulveda <masepulvedar@uqvirtual.edu.co>
2023-09-22 21:17:46 -0500

    loading skeletons añadidos

---
+++ src/Components/TodosError.js
@@ -0,0 +1,12 @@
+
+import '../Styles/TodosError.css';
+
+
+function  TodosError () {
+    return (
+        <p>Error al cargar los Todo's</p>
+    );
+}
+/* <FontAwesomeIcon icon={faCirclePlus} style={{color: "#4180ec", fontWeight:"bold"}} /> */
+
+export { TodosError }; 
